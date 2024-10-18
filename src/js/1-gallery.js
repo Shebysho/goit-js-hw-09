baf78f7dc50909b2b18f6a88e350c67e2e6b52cf
Aargo911 <jekaboy2019@gmail.com>
2024-10-18 09:30:34 +0300

    two-commit

---
+++ src/js/1-gallery.js
@@ -0,0 +1,26 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryMarkup = images.map(({ preview, original, description }) => `
+  <li class="gallery-item">
+    <a class="gallery-link" href="${original}">
+      <img 
+        class="gallery-image" 
+        src="${preview}" 
+        alt="${description}" 
+      />
+    </a>
+  </li>
+`).join('');
+
+galleryList.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom'
+});
